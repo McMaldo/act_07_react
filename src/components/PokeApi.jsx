9f6c1b7331c1effbdaeb312408e76691533257da
Maldonado Pablo Ignacio <164094204+McMaldo@users.noreply.github.com>
2024-05-26 16:46:29 -0300

    cambios en las tarjetitas

--- src/components/PokeApi.jsx
+++ src/components/PokeApi.jsx
@@ -1,27 +1,31 @@
 import { useState } from 'react'
 import './PokeApi.css'
 
-const pokemon = "mew";
 const dataPoke = async (pkm) =>{
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkm}`);
   const results = await data.json();
   return results;
 }
 
+const pokemon = "rayquaza";
 const data = await dataPoke(pokemon);
 export default function PokeApi() {
   return (
   <article className='pkm'>
-    <div>
-      <h3>{data.id}</h3>
-      <h3 className='pkmName'>{data.name}</h3>
+    <div className='pkm-desc'>
+      <p>
+        <h3>{data.id}</h3>
+        <h3 className='pkmName'>{data.name}</h3>
+      </p>
+      <img src={data.sprites.other.dream_world.front_default}/>
+      <div className="stats">
+        {data.stats.map((sta) => {
+        return (
+          <p><div>{sta.stat.name}:</div><div>{sta.base_stat}</div></p>
+        );
+        })}
+      </div>
     </div>
-    <img src={data.sprites.other.dream_world.front_default}/><br/>
-    {data.stats.map((sta) => {
-    return (
-      <div><p>{sta.stat.name}:</p><p>{sta.base_stat}</p></div>
-    );
-    })}
   </article>
   )
 }
